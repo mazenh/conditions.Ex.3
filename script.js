a35605a81a16f9f8feb5c3a0fae85a36ceafa21f
mazen hosaisy <mazen hosaisy>
2016-08-24 18:59:39 +0300

    first

---
+++ script.js
@@ -0,0 +1,30 @@
+function israel(){
+	document.getElementById("israel").className = "styleTwo";
+}
+function israelTwo(){
+	document.getElementById("israel").className = "p";
+}
+function usa(){
+	document.getElementById("usa").className = "styleTwo";
+}
+function usaTwo(){
+	document.getElementById("usa").className = "p";
+}
+function italy(){
+	document.getElementById("italy").className = "styleTwo";
+}
+function italyTwo(){
+	document.getElementById("italy").className = "p";
+}
+function holland(){
+	document.getElementById("holland").className = "styleTwo";
+}
+function hollandTwo(){
+	document.getElementById("holland").className = "p";
+}
+function france(){
+	document.getElementById("france").className = "styleTwo";
+}
+function franceTwo(){
+	document.getElementById("france").className = "p";
+}
